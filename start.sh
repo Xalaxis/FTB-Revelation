03b55487438231e0ec5fcca99b1cf914a9337fd6
Matthew Wolffsohn <matthew@wolfie.ovh>
2020-12-20 00:35:40 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 bash cleanup.sh
 
-RUN cp -R /tmp/creeperpackinstaller /opt/minecraftftb
+cp -R /tmp/creeperpackinstaller /opt/minecraftftb
 
 # echo "Creating Minecraft EULA file"
-RUN echo "eula=true" > eula.txt
+echo "eula=true" > eula.txt
 bash -c java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx${MAXMEMORY} -Xms${MINMEMORY} -Dfml.queryResult=confirm -jar forge-*.jar nogui
